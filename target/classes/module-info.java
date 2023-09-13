94d3dda15bd43af30fc463b4c9c24b664fadc187
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-13 10:06:48 +0300

    .

--- target/classes/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.tugalsan.api.socket {
-    requires com.tugalsan.api.runnable;
-    requires com.tugalsan.api.callable;
-    requires com.tugalsan.api.thread;
-    requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.string;
-    exports com.tugalsan.api.socket.server;
-}
